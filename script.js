655ccb95cacab19f26037c908829d2f89b557ebe
vaz-lucas <lucas.vazxp@gmail.com>
2025-05-30 19:07:16 -0300

    squares color changing

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ function createGrid() {
   squares = document.querySelectorAll(".gridDiv");
 }
 
+// function randomColor() {
+//   let rgbR = Math.ceil(Math.random() * 255);
+//   let rgbG = Math.ceil(Math.random() * 255);
+//   let rgbB = Math.ceil(Math.random() * 255);
+// }
+
 // Picks up the new square size
 function changeSquareSize() {
   const newSize = prompt("A number from 1-100");
@@ -27,11 +33,15 @@ function changeSquareSize() {
     return;
   }
 
+  let rgbR = Math.ceil(Math.random() * 255);
+  let rgbG = Math.ceil(Math.random() * 255);
+  let rgbB = Math.ceil(Math.random() * 255);
+
   squares.forEach((square) => {
     square.style.height = `${sizeNum}px`;
     square.style.width = `${sizeNum}px`;
+    square.style.backgroundColor = `rgb(${rgbR} ${rgbG} ${rgbB})`;
   });
 }
-
 createGrid();
 sizeBtn.addEventListener("click", changeSquareSize);
